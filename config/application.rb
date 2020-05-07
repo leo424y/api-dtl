bbd4518c6796259376b842f9b70939d7c77bb363
Fly Chang <leo424y@gmail.com>
2020-05-07 17:17:58 +0800

    .

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module DtlApi
   class Application < Rails::Application
-    config.hosts << ENV['ALLOW_DOMAIN']
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.before_configuration do
@@ -17,7 +16,7 @@ module DtlApi
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
-
+    config.hosts << ENV['ALLOW_DOMAIN']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
