522022fd38786c46f0044093680d9cb7920f9a5e
Fly Chang <leo424y@gmail.com>
2020-05-11 15:05:14 +0800

    with subdomain

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module DtlApi
         ENV[key.to_s] = value
       end if File.exists?(env_file)
     end
-    config.hosts << ENV['ALLOW_DOMAIN']
+    config.hosts << (/[a-z0-9]/ + ENV['ALLOW_DOMAIN'])
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
