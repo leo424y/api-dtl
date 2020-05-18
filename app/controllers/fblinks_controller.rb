d64970ab63a45ac818f9cc7380fc11c9a8b4caae
Fly Chang <leo424y@gmail.com>
2020-05-18 12:20:51 +0800

    domain case

--- app/controllers/fblinks_controller.rb
+++ app/controllers/fblinks_controller.rb
@@ -6,14 +6,16 @@ class FblinksController < ApplicationController
     the_date = params[:date] ? params[:date].to_date : Date.today
     @fblinks = Fblink.where("to_date(updated, 'YY-MM-DD') BETWEEN ? AND ?", the_date.beginning_of_day, the_date.end_of_day)
     @fblinks = @fblinks.where("title LIKE ?", "%#{params[:content]}%") if params[:content]
-    @fblinks = @fblinks.where("link_domain LIKE ?", "%#{params[:domain]}%") if params[:domain]
     if params[:listid]
       list =  Ctlist.find_by(creator: params[:listid]) 
       listid = list ? list.listid : params[:listid]
       @fblinks = @fblinks.where("list LIKE ?", "#{listid}")
     end
-
-    @fblinks= @fblinks.group(:link_domain).count.sort_by {|k,v| v}.reverse
+    if params[:domain]
+      @fblinks = @fblinks.where(link_domain: params[:domain])
+    else
+      @fblinks= @fblinks.group(:link_domain).count.sort_by {|k,v| v}.reverse
+    end
     json_response(@fblinks)
   end
 end
