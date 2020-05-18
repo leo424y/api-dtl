da2c057da8478bfb18aaad31fa34636acafaa75c
Fly Chang <leo424y@gmail.com>
2020-05-18 11:01:02 +0800

    listid filter

--- app/controllers/fblinks_controller.rb
+++ app/controllers/fblinks_controller.rb
@@ -7,6 +7,10 @@ class FblinksController < ApplicationController
     @fblinks = Fblink.where("to_date(updated, 'YY-MM-DD') BETWEEN ? AND ?", the_date.beginning_of_day, the_date.end_of_day)
     @fblinks = @fblinks.where("title LIKE ?", "%#{params[:content]}%") if params[:content]
     @fblinks = @fblinks.where("link_domain LIKE ?", "%#{params[:domain]}%") if params[:domain]
+    if params[:listid]
+      listid = Ctlist.find_by(creator: params[:listid]).listid || params[:listid]
+      @fblinks = @fblinks.where("list LIKE ?", "%#{listid}%")
+    end
 
     @fblinks= @fblinks.group(:link_domain).count.sort_by {|k,v| v}.reverse
     json_response(@fblinks)
