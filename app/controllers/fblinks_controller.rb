ee18eb9cc404792d693afe3d5b6512100640b0be
Fly Chang <leo424y@gmail.com>
2020-05-18 13:11:08 +0800

    list could be name

--- app/controllers/fblinks_controller.rb
+++ app/controllers/fblinks_controller.rb
@@ -7,9 +7,9 @@ class FblinksController < ApplicationController
     end_date = params[:end_date] ? params[:end_date].to_date : Date.today
     @fblinks = Fblink.where("to_date(updated, 'YY-MM-DD') BETWEEN ? AND ?", start_date.beginning_of_day, end_date.end_of_day)
     @fblinks = @fblinks.where("title LIKE ?", "%#{params[:content]}%") if params[:content]
-    if params[:listid]
-      list =  Ctlist.find_by(creator: params[:listid]) 
-      listid = list ? list.listid : params[:listid]
+    if params[:list]
+      list =  Ctlist.find_by(creator: params[:list]) 
+      listid = list ? list.listid : params[:list]
       @fblinks = @fblinks.where("list LIKE ?", "#{listid}")
     end
     if params[:domain]
