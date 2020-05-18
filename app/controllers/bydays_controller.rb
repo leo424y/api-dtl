fa8ad030cfc2b7f2f02559ad93bcc04e431eb0d8
Fly Chang <leo424y@gmail.com>
2020-05-18 13:41:28 +0800

    move daily counts to by days

--- app/controllers/bydays_controller.rb
+++ app/controllers/bydays_controller.rb
@@ -4,7 +4,23 @@ class BydaysController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   include Response
-  def index
+
+  def index 
+    start_date = params[:start_date] ? params[:start_date].to_date : Date.today
+    end_date = params[:end_date] ? params[:end_date].to_date : Date.today
+    @fblinks = Fblink.where("to_date(updated, 'YY-MM-DD') BETWEEN ? AND ?", start_date.beginning_of_day, end_date.end_of_day)
+    if params[:list]
+      list =  Ctlist.find_by(creator: params[:list]) 
+      listid = list ? list.listid : params[:list]
+      @fblinks = @fblinks.where("list LIKE ?", "#{listid}")
+    end
+    @fblinks = @fblinks.where(link_domain: params[:domain]) if params[:domain]
+
+    @fblinks= @fblinks.group(:link_domain).count.sort_by {|k,v| v}.reverse
+    json_response(@fblinks)
+  end
+
+  def indexx
     @byday = (params[:name] || params[:date]) ? Byday.all : Byday.none
     @byday = @byday.where(created_at: params[:date].to_date.beginning_of_day..params[:date].to_date.end_of_day) if params[:date]
     @byday = @byday.where("name LIKE ?", "%#{params[:name]}%") if params[:name]
--- app/controllers/fblinks_controller.rb
+++ app/controllers/fblinks_controller.rb
@@ -7,16 +7,14 @@ class FblinksController < ApplicationController
     end_date = params[:end_date] ? params[:end_date].to_date : Date.today
     @fblinks = Fblink.where("to_date(updated, 'YY-MM-DD') BETWEEN ? AND ?", start_date.beginning_of_day, end_date.end_of_day)
     @fblinks = @fblinks.where("title LIKE ?", "%#{params[:content]}%") if params[:content]
+    @fblinks = @fblinks.where("link_domain LIKE ?", "%#{params[:content]}%") if params[:domain]
+
     if params[:list]
       list =  Ctlist.find_by(creator: params[:list]) 
       listid = list ? list.listid : params[:list]
       @fblinks = @fblinks.where("list LIKE ?", "#{listid}")
     end
-    if params[:domain]
-      @fblinks = @fblinks.where(link_domain: params[:domain])
-    else
-      @fblinks= @fblinks.group(:link_domain).count.sort_by {|k,v| v}.reverse
-    end
+
     json_response(@fblinks)
   end
 end
