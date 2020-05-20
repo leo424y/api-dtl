887254924362877eb3d86eca9ae93ac031fbca06
Fly Chang <leo424y@gmail.com>
2020-05-20 18:04:54 +0800

    log it

--- app/controllers/pablo_controller.rb
+++ app/controllers/pablo_controller.rb
@@ -4,6 +4,7 @@ class PabloController < ApplicationController
   include Response
 
   def index 
+    log_search
     respond_to do |format|
       format.json { render json: Pablo.count_result(params) }
       format.csv { send_data a_to_csv(Pablo.result(params), 'content creator domain pubTime siteName title url'), filename: "pablo-#{Date.today}-#{params[:q]}-from-#{params[:start_date]}-to-#{params[:end_date]}.csv" }
