d23d79a91526d7729bc22287bf504610d7e128a5
Fly Chang <leo424y@gmail.com>
2020-05-20 17:55:12 +0800

    pablo db loop download by date

---
+++ app/controllers/pablo_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PabloController < ApplicationController
+  include Response
+
+  def index 
+    respond_to do |format|
+      format.json { render json: Pablo.count_result(params) }
+      format.csv { send_data a_to_csv(Pablo.result(params), 'content creator domain pubTime siteName title url'), filename: "pablo-#{Date.today}-#{params[:q]}-from-#{params[:start_date]}-to-#{params[:end_date]}.csv" }
+    end  
+  end
+end
--- app/models/pablo.rb
+++ app/models/pablo.rb
@@ -1,24 +1,43 @@
 class Pablo < ApplicationRecord
-  def self.result (params)
-      require 'net/http'
-      uri = URI.parse("#{ENV['PABLO_API']}&keyword=#{URI.escape(params[:q])}&position=1&emotion=1&startTime=#{params[:start_date]}&endTime=#{params[:end_date]}&pageIndex=1&pageRows=99999")
-      begin
-          response = Timeout::timeout(28) { Net::HTTP.get_response(uri)}
-          rows_hash = JSON.parse(response.body)['body']['list']
-          # {
-          #         source: 'pablo',
-          #         params: params,
-          #         status: 200,
-          #         count: rows_hash.count, 
-          #         posts: rows_hash,
-          # }
-          rows_hash
-      rescue
-          {
-                  source: 'pablo',
-                  params: params,
-                  status: 'timeout',
-          }
-      end
+  def self.result params
+    begin
+        @rows = []
+        dates = params[:start_date].to_date..params[:end_date].to_date
+        dates.each{ |x|
+          date = x.strftime("%Y-%m-%d")
+          response = Timeout::timeout(30) { Net::HTTP.get_response(pablo_uri_date params[:q], date)}
+          @rows += JSON.parse(response.body)['body']['list']  
+        }
+        @rows
+    rescue => error
+        {
+            status: error,
+        }
+    end
+  end
+
+  def self.count_result params 
+    begin
+    response = Timeout::timeout(30) { Net::HTTP.get_response(pablo_uri params)}
+    counts = JSON.parse(response.body)['body']['totalRows']
+    {
+        status: 'ok',
+        source: 'pablo',
+        params: params,
+        counts: counts,
+    }
+    rescue => error
+    {
+        status: error,
+    }
+    end
+  end
+
+  def self.pablo_uri params
+    URI.parse("#{ENV['PABLO_API']}&keyword=#{URI.escape(params[:q])}&position=1&emotion=1&startTime=#{params[:start_date]}&endTime=#{params[:end_date]}&pageIndex=1&pageRows=50")    
+  end
+
+  def self.pablo_uri_date q, date
+    URI.parse("#{ENV['PABLO_API']}&keyword=#{URI.escape(q)}&position=1&emotion=1&startTime=#{date}&endTime=#{date}&pageIndex=1&pageRows=50")    
   end
 end
