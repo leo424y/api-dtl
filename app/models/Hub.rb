302c60cb77d9760ed53a8c1f20b4ff0b06a5db78
Fly Chang <leo424y@gmail.com>
2020-05-22 16:50:34 +0800

    document for usage!

--- app/models/Hub.rb
+++ app/models/Hub.rb
@@ -6,11 +6,13 @@ class Hub < ApplicationRecord
           platform: "cofact", 
           url: "https://api.doublethinklab.org/cofact?q=#{params[:q]}",
           download: "https://api.doublethinklab.org/cofact.csv?q=#{params[:q]}",
+          document: "https://github.com/doublethinklab/API/wiki/cofact"
         },
         {
           platform: "fblinks", 
           url: "https://api.doublethinklab.org/fblinks?q=#{params[:q]}",
           download: "https://api.doublethinklab.org/fblinks.csv?q=#{params[:q]}",
+          document: "https://github.com/doublethinklab/API/wiki/fblinks",
         }
         ] if params[:q]
     result+=[
@@ -18,11 +20,13 @@ class Hub < ApplicationRecord
           platform: "pablo", 
           url: "https://api.doublethinklab.org/pablo?q=#{params[:q]}&start_date=#{params[:start_date]}&end_date=#{params[:end_date]}",
           download: "https://api.doublethinklab.org/pablo.csv?q=#{params[:q]}&start_date=#{params[:start_date]}&end_date=#{params[:end_date]}",
+          document: "https://github.com/doublethinklab/API/wiki/pablo",
         },
         { 
           platform: "crowdtangle", 
           url: "https://api.doublethinklab.org/crowdtangle?q=#{params[:q]}&start_date=#{params[:start_date]}&end_date=#{params[:end_date]}",
           download: "https://api.doublethinklab.org/crowdtangle.csv?q=#{params[:q]}&start_date=#{params[:start_date]}&end_date=#{params[:end_date]}",
+          document: "https://github.com/doublethinklab/API/wiki/crowdtangle",
         },
         ] if params[:start_date] && params[:end_date]
     result
