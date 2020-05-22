2d46a61f752c84dd5cc367ce67b0cdf3e39f6a98
Fly Chang <leo424y@gmail.com>
2020-05-22 15:22:57 +0800

    more data

--- config/application.rb
+++ config/application.rb
@@ -110,7 +110,7 @@ module Rumors
               ListArticles(
                 filter: { moreLikeThis: { like: $text } }
                 orderBy: [{ _score: DESC }]
-                first: 200
+                first: 300
               ) {
                 edges {
                   node {
