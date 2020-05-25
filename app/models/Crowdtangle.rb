b7549514198e1dd2930cab3ca35a92fa64bef013
Fly Chang <leo424y@gmail.com>
2020-05-25 18:08:16 +0800

    interaction rate matters

--- app/models/Crowdtangle.rb
+++ app/models/Crowdtangle.rb
@@ -4,7 +4,7 @@ class Crowdtangle < ApplicationRecord
   def self.search(params)
     require 'net/https'
     token = ENV['CT_TOCKEN']
-    uri = URI("https://api.crowdtangle.com/posts/search/?token=#{token}&searchTerm=#{URI.escape params[:q]}&startDate=#{params[:start_date]}&endDate=#{(params[:end_date].to_date + 1.day).strftime('%Y-%m-%d')}&sortBy=date&count=100")
+    uri = URI("https://api.crowdtangle.com/posts/search/?token=#{token}&searchTerm=#{URI.escape params[:q]}&startDate=#{params[:start_date]}&endDate=#{(params[:end_date].to_date + 1.day).strftime('%Y-%m-%d')}&sortBy=interaction_rate&count=100")
     request = Net::HTTP.get_response(uri)
     rows_hash = JSON.parse(request.body)['message'] || JSON.parse(request.body)['result']['posts']
     rows_hash
