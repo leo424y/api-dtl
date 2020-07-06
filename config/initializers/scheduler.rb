d7df0d1f155569dee48d9a4fccf73cf298e7dd9c
Fly Chang <leo424y@gmail.com>
2020-07-06 15:13:24 +0800

    scheduler

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,24 +1,24 @@
-# #
-# # config/initializers/scheduler.rb
+#
+# config/initializers/scheduler.rb
 
-# require 'rufus-scheduler'
+require 'rufus-scheduler'
 
-# # Let's use the rufus-scheduler singleton
-# #
-# s = Rufus::Scheduler.singleton
+# Let's use the rufus-scheduler singleton
+#
+s = Rufus::Scheduler.singleton
 
-# s.every '1m' do
-#   begin
-#     Crowdtangle.ct_api_import 
-#   rescue => exception
-#     p exception
-#   end
-# end 
+s.every '1m' do
+  begin
+    Crowdtangle.ct_api_import 
+  rescue => exception
+    p exception
+  end
+end 
 
-# s.every '30m' do
-#   begin
-#     Gene.news_api_import 
-#   rescue => exception
-#     p exception
-#   end
-# end 
+s.every '30m' do
+  begin
+    Gene.news_api_import 
+  rescue => exception
+    p exception
+  end
+end 
