28ca97de13aa29c060886bc6af4a40bed840e76d
Fly Chang <leo424y@gmail.com>
2020-07-07 14:39:48 +0800

    gene api update

--- app/models/Gene.rb
+++ app/models/Gene.rb
@@ -6,7 +6,7 @@ class Gene < ApplicationRecord
     medias = 'setn,chinatimes,nownews,tvbs,ltn,udn,upmedia,ebc,ctitv,cna,nius,inside,ftv,cnyes,nikkei,pts,yahoo,apple,epochtimes,storm,newtalk,tnl,cmmedia,rfi,bbc,womany'.split(',')
     medias.each do |media|
         begin
-        uri = URI("http://api.analysis.tw/api/news_dump.php?media=#{media}")
+        uri = URI("http://tag.analysis.tw/api/news_dump.php?media=#{media}")
         request = Net::HTTP.get_response(uri)
         rows_hash = JSON.parse(request.body)
         rows_hash.each do |row_hash|
