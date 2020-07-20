e8ad2b7fc2c46ac24e6882f6c2da4ed1e10ce71f
Fly Chang <leo424y@gmail.com>
2020-07-20 11:04:34 +0800

    download link for Google fact check

--- app/controllers/claim_controller.rb
+++ app/controllers/claim_controller.rb
@@ -4,8 +4,15 @@ class ClaimController < ApplicationController
   include Response
 
   def index
+    result = Claim.count_result(params).as_json
     respond_to do |format|
-      format.json { render json: Claim.count_result(params) }
+      format.json { render json: download_link.merge(result) }
+      format.csv do
+        send_data(
+          a_to_csv(result['result'], 'text claimDate claimReview'),
+          filename: name_file(controller_name, params)
+        )
+      end
     end
   end
 end
