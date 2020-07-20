70e3d43ce45daa5f00c213c5b503a6d7eca84943
Fly Chang <leo424y@gmail.com>
2020-07-20 11:23:58 +0800

    csv for domain search

--- app/controllers/domain_controller.rb
+++ app/controllers/domain_controller.rb
@@ -4,8 +4,15 @@ class DomainController < ApplicationController
   include Response
 
   def index
+    result = Domain.count_result(params).as_json
     respond_to do |format|
-      format.json { render json: Domain.count_result(params) }
+      format.json { render json: download_link.merge(result) }
+      format.csv do
+        send_data(
+          a_to_csv(result['result'], 'ctime url title description'),
+          filename: name_file(controller_name, params)
+        )
+      end
     end
   end
 end
