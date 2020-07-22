1b96f2b4aebed0d29d899dc4b171c45e53c1bfc8
Fly Chang <leo424y@gmail.com>
2020-07-22 13:51:55 +0800

    add title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-    def icon_url url
-      image_tag "https://s2.googleusercontent.com/s2/favicons?domain_url=#{URI(url).host}", size: "16x16"
-    end
+  def icon_url(url)
+    image_tag "https://s2.googleusercontent.com/s2/favicons?domain_url=#{URI(url).host}", size: '16x16'
+  end
+
+  def to_title(text)
+    content_for :title, text
+  end
 end
