2903c04d4ff64e46b6baf2aca7bd4a506683678d
Fly Chang <leo424y@gmail.com>
2020-07-23 11:51:08 +0800

    schedule by env

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -7,18 +7,22 @@ require 'rufus-scheduler'
 #
 s = Rufus::Scheduler.singleton
 
-s.every '1m' do
-  begin
-    Crowdtangle.ct_api_import 
-  rescue => exception
-    p exception
-  end
-end 
+if Rails.env != 'development'
+  s.every '1m' do
+    begin
+      Crowdtangle.ct_api_import 
+    rescue => exception
+      p exception
+    end
+  end 
+end
 
-s.every '30m' do
-  begin
-    Gene.news_api_import 
-  rescue => exception
-    p exception
-  end
-end 
+if Rails.env == 'development'
+  s.every '30m' do
+    begin
+      Gene.news_api_import 
+    rescue => exception
+      p exception
+    end
+  end 
+end
