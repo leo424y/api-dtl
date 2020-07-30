58f07ef2b34f091eae9c36f5eca6ce299da82d99
Fly Chang <leo424y@gmail.com>
2020-07-30 17:58:28 +0800

    wiki uri

--- app/models/wikipedia.rb
+++ app/models/wikipedia.rb
@@ -2,7 +2,8 @@
 
 class Wikipedia < ApplicationRecord
   def self.count_result(params)
-    api_uri = URI "https://zh.wikipedia.org/w/api.php?action=query&list=search&srsearch=#{URI.escape params[:q]}&utf8&format=json"
+    p = URI.encode_www_form(srsearch: params[:q])
+    api_uri = URI "https://zh.wikipedia.org/w/api.php?action=query&list=search&#{p}&utf8&format=json"
     response = Timeout.timeout(30) { Net::HTTP.get_response(api_uri) }
     result = JSON.parse(response.body)['query']['search']
     count = result ? result.count : 0
