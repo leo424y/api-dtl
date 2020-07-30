584a8eda14b199505a81fcceeb68e072e790e66e
Fly Chang <leo424y@gmail.com>
2020-07-30 17:24:42 +0800

    URL.escape

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def icon_url(url)
+    url = url.split('/')[0..2].join('/')
     image_tag "https://s2.googleusercontent.com/s2/favicons?domain_url=#{URI(url).host}", size: '16x16'
   end
 
