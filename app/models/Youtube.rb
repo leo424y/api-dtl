e0e420791c14f7d1ae0508aee694dc41fd72e82d
Fly Chang <leo424y@gmail.com>
2020-08-05 11:48:36 +0800

    fix json issue

--- app/models/Youtube.rb
+++ app/models/Youtube.rb
@@ -3,14 +3,14 @@
 class Youtube < ApplicationRecord
   def self.count_result(params)
     result = Timeout.timeout(30) { 
-      JSON.parse(%x(youtube-dl --default-search "ytsearch999:" --skip-download -J #{params[:q]}))['entries'].sort_by { |h| h['upload_date'] }
-    }    
-    count = result ? result.count : 0 
+      JSON.parse(%x(youtube-dl --default-search "ytsearch999:" --skip-download -J #{params[:q]}))
+    } 
+
     {
       status: 'ok',
       params: params,
-      count: count,
-      result: result
+      count: result['entries'] ? result['entries'].count : 0,
+      result: result['entries'].sort_by { |h| h['upload_date'] }
     }
   rescue StandardError => e
     {
