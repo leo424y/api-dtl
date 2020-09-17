3a10daa1096781ca1cc1112753ddfd1cd9b0dded
Fly Chang <leo424y@gmail.com>
2020-09-17 16:36:55 +0800

    write data to yt

--- app/models/Youtuber.rb
+++ app/models/Youtuber.rb
@@ -7,7 +7,13 @@ class Youtuber < ApplicationRecord
     result = Timeout.timeout(50) { 
       JSON.parse(Net::HTTP.get_response(uri).body)
     } 
-
+    result.each do |r|
+      begin
+        yt_to_dtl r, params[:q]
+      rescue => exception
+        p exception
+      end
+    end
     {
       status: 'ok',
       params: params,
@@ -18,5 +24,54 @@ class Youtuber < ApplicationRecord
     {
       status: e
     }
+  end
+
+
+  def self.yt_to_dtl r, search   
+    host = "http://a.doublethinklab.org/graphql?"
+    gql = <<~GQL
+    mutation {
+      createDtl(input: {
+        source: "dtlyt"
+        url: "#{r['url']}"
+        channelId: "#{r['channelId']}"
+        channelName: "#{r['channelTitle']}"
+        link: ""
+        domain: "youtube.com"
+        title: "#{URI.encode_www_form_component r['title']}"
+        description: "#{URI.encode_www_form_component r['description']}"
+        content: ""
+        pubTime: "#{r['publishedAt']}"
+        search: "#{search}"
+      }) {
+        dtl {
+          source
+          url
+          id
+          uuid
+          platformId
+          channelId
+          channelName
+          link
+          domain
+          title
+          description
+          content
+          pubTime
+          search
+        }
+        errors
+      }
+    }
+    GQL
+    body = {
+      query: gql,
+    }
+
+    HTTParty.post(
+      host,
+      body: body.to_json,
+      headers: {'Content-Type': 'application/json',}
+    )
   end
 end
