be7eb959f2bffdb9e3863e025599eece4b6e186d
Fly Chang <leo424y@gmail.com>
2020-09-18 13:47:26 +0800

    to check the search history

--- app/controllers/hub_controller.rb
+++ app/controllers/hub_controller.rb
@@ -42,11 +42,13 @@ class HubController < ApplicationController
 
   ################ page, group, profile should be delay 60s each, in case it hit the API limit 
   def hub_crowdtanglego_run
-    p = URI.encode_www_form(q: params[:q], start_date: params[:start_date], end_date: params[:end_date])
-    uri = URI("https://#{ENV['CTCSVHOST']}/facebooks?#{p}")
-    get_ct_count = Net::HTTP.get_response(uri).body.to_i
-    @ct_post_count = get_ct_count.is_a?(Numeric) ? get_ct_count : 0
-    render partial: "hub_crowdtanglego_run" 
+    # if !Searchword.where(word: params[:q], start_date: params[:start_date], end_date: params[:end_date])
+      p = URI.encode_www_form(q: params[:q], start_date: params[:start_date], end_date: params[:end_date])
+      uri = URI("https://#{ENV['CTCSVHOST']}/facebooks?#{p}")
+      get_ct_count = Net::HTTP.get_response(uri).body.to_i
+      @ct_post_count = get_ct_count.is_a?(Numeric) ? get_ct_count : 0
+      render partial: "hub_crowdtanglego_run" 
+    # end
   end
 
   def hub_crowdtanglego
