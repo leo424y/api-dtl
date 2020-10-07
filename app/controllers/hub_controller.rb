ccea820168c6b1e8a9ef125dd2082c357c3a4512
Fly Chang <leo424y@gmail.com>
2020-10-07 12:53:57 +0800

    ✨cache fblinks to dtl

--- app/controllers/hub_controller.rb
+++ app/controllers/hub_controller.rb
@@ -11,23 +11,23 @@ class HubController < ApplicationController
     end
   end
 
-  def hub_wikipedia 
+  def hub_wikipedia
     @hub_wikipedia = Wikipedia.count_result(params)
     render partial: "hub_wikipedia"
   end
 
   def hub_claim
     @hub_claim = Claim.count_result(params).as_json['result']
-    @gf_count = @hub_claim.count 
+    @gf_count = @hub_claim.count
     @gf_dl = download_link_of 'claim'
-    render partial: "hub_claim" 
+    render partial: "hub_claim"
   end
 
   def hub_cofact
     @hub_cofact = Cofact.count_result(params).as_json['posts_by_date']
     @cf_count = @hub_cofact.count
     @cf_dl = download_link_of 'cofact'
-    render partial: "hub_cofact" 
+    render partial: "hub_cofact"
   end
 
   def hub_crowdtangle
@@ -37,10 +37,10 @@ class HubController < ApplicationController
 
     @ct_count = crowdtangle['count']
     @ct_dl = download_link_of 'crowdtangle'
-    render partial: "hub_crowdtangle" 
+    render partial: "hub_crowdtangle"
   end
 
-  ################ page, group, profile should be delay 60s each, in case it hit the API limit 
+  ################ page, group, profile should be delay 60s each, in case it hit the API limit
   def hub_crowdtanglego_run
     fix_query_date
     # if !Searchword.where(word: params[:q], start_date: params[:start_date], end_date: params[:end_date])
@@ -48,7 +48,7 @@ class HubController < ApplicationController
     uri = URI("https://#{ENV['CTCSVHOST']}/facebooks?#{p}")
     get_ct_count = Net::HTTP.get_response(uri).body.to_i
     @ct_post_count = get_ct_count.is_a?(Numeric) ? get_ct_count : 0
-    render partial: "hub_crowdtanglego_run" 
+    render partial: "hub_crowdtanglego_run"
     # end
   end
 
@@ -56,7 +56,7 @@ class HubController < ApplicationController
     @crowdtanglego = Crowdtanglego.count_result(params).as_json['result']
     # @crowdtanglego = data_compact_host @crowdtanglego, 'link'
     @ctgo_dl = download_link_of 'crowdtanglego'
-    render partial: "hub_crowdtanglego" 
+    render partial: "hub_crowdtanglego"
   end
 
   def hub_crowdtangle_page
@@ -68,7 +68,7 @@ class HubController < ApplicationController
 
     @ct_count_page = crowdtangle['count']
     @ct_dl_page = download_link_of 'crowdtangle'
-    render partial: "hub_crowdtangle_page" 
+    render partial: "hub_crowdtangle_page"
   end
 
   def hub_crowdtangle_group
@@ -81,7 +81,7 @@ class HubController < ApplicationController
 
     @ct_count_group = crowdtangle['count']
     @ct_dl_group = download_link_of 'crowdtangle'
-    render partial: "hub_crowdtangle_group" 
+    render partial: "hub_crowdtangle_group"
   end
 
   def hub_crowdtangle_profile
@@ -94,7 +94,7 @@ class HubController < ApplicationController
 
     @ct_count_profile = crowdtangle['count']
     @ct_dl_profile = download_link_of 'crowdtangle'
-    render partial: "hub_crowdtangle_profile" 
+    render partial: "hub_crowdtangle_profile"
   end
 
   def hub_crowdtangle_instagram
@@ -106,7 +106,7 @@ class HubController < ApplicationController
 
     @ct_count_instagram = crowdtangle['count']
     @ct_dl_instagram = download_link_of 'crowdtangle'
-    render partial: "hub_crowdtangle_instagram" 
+    render partial: "hub_crowdtangle_instagram"
   end
 
   def hub_crowdtangle_reddit
@@ -118,18 +118,18 @@ class HubController < ApplicationController
 
     @ct_count_reddit = crowdtangle['count']
     @ct_dl_reddit = download_link_of 'crowdtangle'
-    render partial: "hub_crowdtangle_reddit" 
+    render partial: "hub_crowdtangle_reddit"
   end
   ################
 
 
   def hub_pablo
-    @hub_pablo = Pablo.count_result(params).as_json['posts_by_date'] 
+    @hub_pablo = Pablo.count_result(params).as_json['posts_by_date']
     @hub_pablo = data_compact @hub_pablo, 'siteName'
 
     @dt_count = @hub_pablo.count
     @dt_dl = download_link_of 'pablo'
-    render partial: "hub_pablo" 
+    render partial: "hub_pablo"
   end
 
   def hub_pablol
@@ -174,35 +174,38 @@ class HubController < ApplicationController
   def hub_youtuber
     @hub_youtuber = Youtuber.count_result(params).as_json['result']
     @ytr_dl = download_link_of 'youtuber'
-    @ytr_count = @hub_youtuber.count 
+    @ytr_count = @hub_youtuber.count
     render partial: "hub_youtuber"
   end
 
   def hub_fblinks
     fblink = count_record(set_filter(Fblink.all)).as_json
     @hub_fblink = fblink['posts_by_date']
+    @hub_fblink.each do |f|
+      fb_to_dtl f, params[:q]
+    end
     @hub_fblink = data_compact @hub_fblink, 'link_domain'
     @fb_count = fblink['count']
     @fb_dl = download_link_of 'fblinks'
     render partial: "hub_fblinks"
-  end  
+  end
 
   def hub_dtl
     sleep 70
     @hub_dtl = Dtl.count_result(params).as_json['result'].reverse
     @dtl_dl = download_link_of 'dtl'
-    @dtl_count = @hub_dtl.count 
+    @dtl_count = @hub_dtl.count
     render partial: "hub_dtl"
   end
 
-  def hub_datacount 
+  def hub_datacount
     render partial: "hub_datacount"
-  end   
-  
-  private 
+  end
+
+  private
   def data_compact_host data, field
     items = []
-    data.map do |x| 
+    data.map do |x|
       uri = x[field].split('/')[0..2].join('/')
       unless items.include? uri
         items << uri
@@ -213,11 +216,59 @@ class HubController < ApplicationController
 
   def data_compact data, field
     items = []
-    data.map do |x| 
+    data.map do |x|
       unless items.include? x[field]
         items << x[field]
         x
       end
     end.compact
   end
+
+  def self.fb_to_dtl r, search
+    host = "http://a.doublethinklab.org/graphql?"
+    gql = <<~GQL
+    mutation {
+      createDtl(input: {
+        source: "dtlfba"
+        url: "#{r['url']}"
+        channelId: "#{r['platform_id']}"
+        channelName: "#{r['platform_name']}"
+        link: "#{r['link']}"
+        domain: "#{r['link_domain']}"
+        title: "#{URI.encode_www_form_component r['title']}"
+        description: ""
+        content: ""
+        pubTime: "#{r['date']}"
+        search: "#{search}"
+      }) {
+        dtl {
+          source
+          url
+          id
+          uuid
+          platformId
+          channelId
+          channelName
+          link
+          domain
+          title
+          description
+          content
+          pubTime
+          search
+        }
+        errors
+      }
+    }
+    GQL
+    body = {
+      query: gql,
+    }
+    HTTParty.post(
+      host,
+      body: body.to_json,
+      headers: {'Content-Type': 'application/json',}
+    )
+  end
+
 end
