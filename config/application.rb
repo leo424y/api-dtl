a37be903d29bf13703830e5ca1134514a65167b6
Fly Chang <leo424y@gmail.com>
2020-10-07 17:32:37 +0800

    🐛 timezone issue

--- config/application.rb
+++ config/application.rb
@@ -10,14 +10,18 @@ module DtlApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zome = 'Taipei'
+    config.active_record.default_timezone = :local
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
     end
     config.hosts << /[a-z0-9]+\.#{ENV['ALLOW_DOMAIN']}/
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
